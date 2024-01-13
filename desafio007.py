b55b38a2ef30d0d8f0220224b826e97dd8d2ce6a
PriChiafarelo <priscilachiafarelo@gmail.com>
2024-01-13 13:31:30 -0300

    primeiro commit

---
+++ desafio007.py
@@ -0,0 +1,6 @@
+"""Desenvolva um programa que leia as duas notas de um aluno, calcule e mostra a sua média"""
+
+n1 = float(input('Digite a nota da N1: '));
+n2 = float(input('Digite a nota da N2: '));
+print('A média das notas é {:.1f}'.format((n1 + n2)/2))
+
